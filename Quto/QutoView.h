9ca44338a72ac1552e092b34e337e5b7fb74f63d
Mohamed ELMeseery <eng.m.elmeseery@gmail.com>
2019-01-06 13:05:37 +0200

    Initial Commit

---
+++ Quto/QutoView.h
@@ -0,0 +1,13 @@
+//
+//  QutoView.h
+//  Quto
+//
+//  Created by Mohamed EL Meseery on 1/6/19.
+//  Copyright © 2019 Meseery. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface QutoView : ScreenSaverView
+
+@end
---
+++ Quto/QutoView.m
@@ -0,0 +1,52 @@
+//
+//  QutoView.m
+//  Quto
+//
+//  Created by Mohamed EL Meseery on 1/6/19.
+//  Copyright © 2019 Meseery. All rights reserved.
+//
+
+#import "QutoView.h"
+
+@implementation QutoView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
